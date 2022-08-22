fdf871b58919ab699ad356a2ec7a2dd341671f11
Haydn <haydnmey@gmail.com>
2022-08-22 12:57:55 +0200

    added body-parsing middleware

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send("Welcome to your Budget Planner! If you'd like to see all of your envelopes, navigate to /envelopes in your URL");
 });
 
+// Parsing middleware that parses request bodies into JSON
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 app.listen(PORT, () => {
   console.log(`Personal Budget app listening on port ${PORT}`);
 });
