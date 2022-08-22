d77e2491162ecb418118df22b3c27708f0b86388
Haydn <haydnmey@gmail.com>
2022-08-22 15:02:51 +0200

    Mounted my envelopeRouter to be used with all /envelopes endpoints

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send("Welcome to your Budget Planner! If you'd like to see all of your envelopes, navigate to /envelopes in your URL");
+  res.send("Welcome to your Budget Planner! Go ahead and create your first envelope!");
 });
 
 // Parsing middleware that parses request bodies into JSON
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+// Mounting the envelopeRouter
+const envelopeRouter = require('./Server/Routers/envelopes');
+app.use('/envelopes', envelopeRouter);
 
 app.listen(PORT, () => {
   console.log(`Personal Budget app listening on port ${PORT}`);
