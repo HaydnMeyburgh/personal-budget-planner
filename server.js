efc77b9fe5dd1cd8dcc1f65c1b895107c2e1444a
Haydn <haydnmey@gmail.com>
2022-08-26 17:08:16 +0200

    removed unnecessary boilerplate and commented out code that wasnt being used

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send("Welcome to your Budget Planner! Go ahead and create your first envelope!");
-});
-
 // Parsing middleware that parses request bodies into JSON
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -13,9 +9,6 @@ app.use(bodyParser.json());
 // Mounting the envelopeRouter
 const envelopeRouter = require('./Server/Routers/envelopes');
 app.use('/envelopes', envelopeRouter);
-
-const transfersRouter = require('./Server/Routers/transfers');
-app.use('/envelopes/transfer', transfersRouter);
 
 app.listen(PORT, () => {
   console.log(`Personal Budget app listening on port ${PORT}`);
