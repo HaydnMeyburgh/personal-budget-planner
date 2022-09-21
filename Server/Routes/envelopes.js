ef6a8b36bd85d3ab9f8131c3abcf9739c427fec9
Haydn <haydnmey@gmail.com>
2022-09-21 20:18:17 +0200

    added comments and removed old util functions

--- Server/Routes/envelopes.js
+++ Server/Routes/envelopes.js
@@ -9,16 +9,17 @@ const {
   transferBudget
 } = require('../controllers/envelopes.controllers');
 
-envelopeRouter.post('/', createEnvelope); 
-
-envelopeRouter.get('/', getAllEnvelopes); 
-
-envelopeRouter.get('/:envelopeId', getEnvelopeById); 
-
-envelopeRouter.delete('/:envelopeId', deleteEnvelope); 
-
-envelopeRouter.put('/:envelopeId', updateEnvelope); 
-
+// Create a new envelope
+envelopeRouter.post('/', createEnvelope);
+// Fetch all envelopes 
+envelopeRouter.get('/', getAllEnvelopes);
+// Fetch an envelope by id 
+envelopeRouter.get('/:envelopeId', getEnvelopeById);
+// Delete an envelope by id
+envelopeRouter.delete('/:envelopeId', deleteEnvelope);
+// Update an envelope 
+envelopeRouter.put('/:envelopeId', updateEnvelope);
+// Transfer amount from one envelope to another envelope 
 envelopeRouter.put('/transfer/:fromId/:toId', transferBudget); 
 
 module.exports = envelopeRouter;
