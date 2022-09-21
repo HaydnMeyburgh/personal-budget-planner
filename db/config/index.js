e593d3adf900914ff3cd2fc4fb1b24511ae27f9f
Haydn <haydnmey@gmail.com>
2022-09-21 11:30:49 +0200

    updated index.js to connect to database correctly

--- db/config/index.js
+++ db/config/index.js
@@ -7,7 +7,8 @@ const db = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PASSWORD
+  port: process.env.DB_PORT
 });
+
 
 module.exports = {db};
