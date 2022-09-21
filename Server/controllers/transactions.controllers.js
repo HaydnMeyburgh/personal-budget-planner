2c6892a31977c031cc48579177fef16e7e1dadb2
Haydn <haydnmey@gmail.com>
2022-09-21 20:21:36 +0200

    setup the boiler plate code for the controllers

---
+++ Server/controllers/transactions.controllers.js
@@ -0,0 +1,64 @@
+const { db } = require('../../db/config/index');
+
+// Fetch all transactions
+const getAllTransactions = async(req, res) => {
+  const query;
+  try {
+
+  } catch(err) {
+    
+  } 
+}
+
+// Fetch transaction by id
+const getTransactionById = async(req, res) => {
+  const query; 
+  try {
+
+  } catch(err) {
+    
+  }
+
+}
+
+// Create Transaction
+const createTransaction = async(req, res) => {
+  const query; 
+  try {
+
+  } catch(err) {
+    
+  }
+
+}
+
+// Update a transaction
+const updateTransaction = async(req, res) => {
+  const query; 
+  try {
+
+  } catch(err) {
+    
+  }
+
+}
+
+// Delete Transaction
+const deleteTransaction = async(req, res) => {
+  const query;
+  
+  try {
+
+  } catch(err) {
+
+  }
+
+}
+
+module.exports = {
+  getAllTransactions,
+  getTransactionById,
+  createTransaction,
+  updateTransaction,
+  deleteTransaction
+}
