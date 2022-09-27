02597e7a54ccd1462c7656738bcf9c10cdd6c823
Haydn <haydnmey@gmail.com>
2022-09-27 16:36:00 +0200

    Created transactions Routes to handle transactions

--- Server/Routes/transactions.js
+++ Server/Routes/transactions.js
@@ -3,20 +3,19 @@ const transactionsRouter = express.Router();
 const {
   getAllTransactions,
   getTransactionById,
-  createTransaction,
   updateTransaction,
-  deleteTransaction
+  deleteTransaction,
 } = require('../controllers/transactions.controllers')
 
 // Get all transactions
-transactionsRouter.get('/transactions', getAllTransactions);
+transactionsRouter.get('/', getAllTransactions);
 // Get transaction by id
-transactionsRouter.get('/transactions/:transactionId', getTransactionById);
-// Create a new transaction
-transactionsRouter.post('/:evelopeId/transactions', createTransaction);
+transactionsRouter.get('/:transactionId', getTransactionById);
 // Update a transaction
-transactionsRouter.put('/transactions/:transactionId', updateTransaction);
+// If it is the transaction amount that is updated then the budget of the corresponding envelope needs to be updated as well to reflect the new transaction amount
+transactionsRouter.put('/:transactionId', updateTransaction);
 // Delete a transaction by id
-transactionsRouter.delete('/transactions/:transactionId', deleteTransaction);
+// If a transaction is deleted then the budget needs to be adjusted for the corresponding envelope (The budget amount should increase)
+transactionsRouter.delete('/:transactionId', deleteTransaction);
 
 module.exports = transactionsRouter;
