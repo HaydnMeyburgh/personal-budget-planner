869cd5e3fb220c4225da319bc359d9db36ad0083
Haydn <haydnmey@gmail.com>
2022-09-27 16:35:18 +0200

    Added new Routes for transactions

--- Server/Routes/envelopes.js
+++ Server/Routes/envelopes.js
@@ -6,8 +6,12 @@ const {
   getEnvelopeById,
   deleteEnvelope,
   updateEnvelope,
-  transferBudget
+  transferBudget,
+  getEnvelopeTransactions,
+  getEnvelopeTransactionById,
+  createTransaction
 } = require('../controllers/envelopes.controllers');
+const { updateTransaction, deleteTransaction } = require('../controllers/transactions.controllers');
 
 // Create a new envelope
 envelopeRouter.post('/', createEnvelope);
@@ -21,5 +25,18 @@ envelopeRouter.delete('/:envelopeId', deleteEnvelope);
 envelopeRouter.put('/:envelopeId', updateEnvelope);
 // Transfer amount from one envelope to another envelope 
 envelopeRouter.put('/transfer/:fromId/:toId', transferBudget); 
+// Get all transactions relating to an envelope
+envelopeRouter.get('/:envelopeId/transactions', getEnvelopeTransactions);
+// Get transaction by id relating to an envelope
+envelopeRouter.get('/:envelopeId/transactions/:transactionId', getEnvelopeTransactionById);
+// Create a new transaction
+// Need to make sure that the corresponding envelopes budget is adjusted to reflect the new transaction
+envelopeRouter.post('/:envelopeId/transactions', createTransaction);
+// Update a transaction
+// If it is the transaction amount that is updated then the budget of the corresponding envelope needs to be updated as well to reflect the new transaction amount
+envelopeRouter.put('/:envelopeId/transactions/:transactionId', updateTransaction);
+// Delete a transaction by id
+// If a transaction is deleted then the budget needs to be adjusted for the corresponding envelope (The budget amount should increase)
+envelopeRouter.delete('/:envelopeId/transactions/:transactionId', deleteTransaction);
 
 module.exports = envelopeRouter;
