a24038ddde249070e7ce8e6bd16b65935448924a
Haydn <haydnmey@gmail.com>
2023-02-14 15:04:36 +0200

    fixed response codes, tidied db queries and conditionals

--- Server/controllers/transactions.controllers.js
+++ Server/controllers/transactions.controllers.js
@@ -1,103 +1,118 @@
-const { db } = require('../../db/config/index');
+const { db } = require("../../db/config/index");
 
 // get all transactions
-const getAllTransactions = async(req, res) => {
+const getAllTransactions = async (req, res) => {
   try {
-    const transactions = await db.query("SELECT * FROM transactions");
-    if(transactions.rowCount < 1) {
+    const transactions = await db.query(
+      "SELECT recipient, amount, date FROM transactions"
+    );
+    if (transactions.rows === 0) {
       return res.status(404).send({
-        message: "No transactions were found"
-      })
+        message: "No transactions were found",
+      });
     }
     res.status(200).send({
       status: "Success",
       message: "Info regarding transactions received",
-      data: transactions.rows
-    }) 
-  } catch(err) {
+      data: transactions.rows,
+    });
+  } catch (err) {
     return res.status(500).send({
-      error: err.message
-    })
-  } 
-}
+      error: err.message,
+    });
+  }
+};
 
 // get transaction by id
-const getTransactionById = async(req, res) => {
-  const {transactionId} = req.params;
+const getTransactionById = async (req, res) => {
+  const { transactionId } = req.params;
   try {
-    const transaction = await db.query("SELECT * FROM transactions WHERE id = $1", [transactionId])
-    if(transaction.rowCount < 1) {
+    const transaction = await db.query(
+      "SELECT recipient, amount, date FROM transactions WHERE id = $1",
+      [transactionId]
+    );
+    if (transaction.rows === 0) {
       return res.status(404).send({
-        message: "Transaction could not be found"
-      })
+        message: "Transaction could not be found",
+      });
     }
     res.status(200).send({
       status: "Success",
-      message: `Successfully found transaction with id - ${transactionId}`,
-      data: transaction.rows[0]
-    })
-  } catch(err) {
+      message: "Successfully found transaction",
+      data: transaction.rows[0],
+    });
+  } catch (err) {
     return res.status(500).send({
-      error: err.message
-    })
+      error: err.message,
+    });
   }
-}
+};
 
 // Update a transaction
-const updateTransaction = async(req, res) => {
-  const {transactionId} = req.params;
-  const {recipient, amount, date} = req.body
+const updateTransaction = async (req, res) => {
+  const { transactionId } = req.params;
+  const { recipient, amount, date } = req.body;
   try {
-    const transaction = await db.query("SELECT * FROM transactions WHERE id = $1", [transactionId]);
-    if(transaction.rowCount < 1) {
+    const transaction = await db.query(
+      "SELECT amount, envelope_id FROM transactions WHERE id = $1",
+      [transactionId]
+    );
+    const updatedTransaction = await db.query(
+      "UPDATE transactions SET recipient = $1, amount = $2, date = $3 WHERE id = $4",
+      [recipient, amount, date, transactionId]
+    );
+    if (updatedTransaction.rows === 0) {
       return res.status(404).send({
-        message: "cannot find transaction"
-      })
+        message:
+          "Transaction could not be updated, ensure correct data is submitted",
+      });
     }
     const currentAmount = transaction.rows[0].amount;
     const envelopeId = transaction.rows[0].envelope_id;
-    await db.query("UPDATE envelopes SET budget = budget - ($1::numeric - $2::numeric) WHERE id = $3", [amount, currentAmount, envelopeId]);
-    await db.query("UPDATE transactions SET recipient = $1, amount = $2, date = $3 WHERE id = $4", [recipient, amount, date, transactionId]);
-    res.status(201).send({
+    await db.query(
+      "UPDATE envelopes SET budget = budget - ($1::numeric - $2::numeric) WHERE id = $3",
+      [amount, currentAmount, envelopeId]
+    );
+    res.status(200).send({
       status: "Success",
-      message: `Successfully updated transaction with id - ${transactionId}`
-    })
-  } catch(err) {
+      message: "Successfully updated transaction",
+    });
+  } catch (err) {
     return res.status(500).send({
-      error: err.message
-    })
+      error: err.message,
+    });
   }
 };
 
 // Delete Transaction
-const deleteTransaction = async(req, res) => {
-  const {transactionId} = req.params;
+const deleteTransaction = async (req, res) => {
+  const { transactionId } = req.params;
   try {
-    const transaction = await db.query("SELECT * FROM transactions WHERE id = $1", [transactionId]);
-    if(transaction.rowCount < 1) {
-      return res.status(404).send({
-        message: "Could not find transaction"
-      })
-    }
+    const transaction = await db.query(
+      "SELECT amount, envelopeId FROM transactions WHERE id = $1",
+      [transactionId]
+    );
     const transactionAmount = transaction.rows[0].amount;
     const envelopeId = transaction.rows[0].envelope_id;
-    await db.query("UPDATE envelopes SET budget = budget + $1 WHERE id = $2", [transactionAmount, envelopeId]); 
+    await db.query("UPDATE envelopes SET budget = budget + $1 WHERE id = $2", [
+      transactionAmount,
+      envelopeId,
+    ]);
     await db.query("DELETE FROM transactions WHERE id = $1", [transactionId]);
     res.status(200).send({
       status: "Success",
-      message: `Successfully deleted transaction with id - ${transactionId}`
-    })
-  } catch(err) {
+      message: "Successfully deleted transaction",
+    });
+  } catch (err) {
     return res.status(500).send({
-      error: err.message
-    })
+      error: err.message,
+    });
   }
-
-}
+};
 
 module.exports = {
   getAllTransactions,
   getTransactionById,
   updateTransaction,
-  deleteTransaction
-}
+  deleteTransaction,
+};
