5f115343fe913f0e53164bac8fb30cc34ea7ad93
Haydn <haydnmey@gmail.com>
2023-02-20 10:10:14 +0200

    added initial css file for main App styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const App = () => {
-  return <h1>Hellow World! I am using React</h1>;
+  const [envelopes, setEnvelopes] = useState();
+
+  useEffect(() => {
+    // I think i rather want to use async await here for the fetch request
+    fetch("http://localhost:3000/api/envelopes", {})
+      .then((res) => res.json())
+      .then((envelopeData) => setEnvelopes(envelopeData));
+  }, []);
+
+  return (
+    <div className="App">
+      <header>
+        <h1 className="title">Budget Planner</h1>
+        <p>
+          View the API{" "}
+          <a href="http://localhost:3000/api/docs" target="_blank">Documentation</a>
+        </p>
+        <p>
+          Use these budget envelopes to track your spending and add transactions
+          when you spend money
+        </p>
+      </header>
+
+      {/* Need to use .map? to extract the object and use the title and budget values */}
+      <footer>
+        <div>
+          Developed by{" "}
+          <a href="https://github.com/HaydnMeyburgh" target="_blank">
+            Haydn
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default App;
--- root/frontend/src/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const App = () => {
-  return <h1>Hellow World! I am using React</h1>;
-};
-
-export default App;
