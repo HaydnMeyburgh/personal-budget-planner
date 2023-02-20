6646acb78d81a25529242198e5e7c40a266bdd4b
Haydn <haydnmey@gmail.com>
2023-02-20 16:16:03 +0200

    Added Routes and Suspense for loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/navbar/navbar";
+const Envelopes = lazy(() => import("./components/envelopes/envelopes"));
+const Transactions = lazy(() =>
+  import("./components/transactions/transactions")
+);
+const EnvelopeDetails = lazy(() =>
+  import("./components/envelopes/envelopeDetails/envelopeDetails")
+);
 
 const App = () => {
-  const [envelopes, setEnvelopes] = useState();
-
-  useEffect(() => {
-    // I think i rather want to use async await here for the fetch request
-    fetch("http://localhost:3000/api/envelopes", {})
-      .then((res) => res.json())
-      .then((envelopeData) => setEnvelopes(envelopeData));
-  }, []);
-
   return (
     <div className="App">
-      <header>
-        <h1 className="title">Budget Planner</h1>
-        <p>
-          View the API{" "}
-          <a href="http://localhost:3000/api/docs" target="_blank">Documentation</a>
-        </p>
-        <p>
-          Use these budget envelopes to track your spending and add transactions
-          when you spend money
-        </p>
-      </header>
-
-      {/* Need to use .map? to extract the object and use the title and budget values */}
-      <footer>
-        <div>
-          Developed by{" "}
-          <a href="https://github.com/HaydnMeyburgh" target="_blank">
-            Haydn
-          </a>
-        </div>
-      </footer>
+      <NavBar />
+      <Suspense fallback={<div className="container">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Envelopes />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/envelopes/:id" element={<EnvelopeDetails />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
