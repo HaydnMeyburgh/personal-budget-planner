60d09bdd8c36f3a5040542ee730cdd9ecd5352df
Haydn <haydnmey@gmail.com>
2023-02-20 10:08:10 +0200

    adjusted apis option file route

--- api/doc/swagger.js
+++ api/doc/swagger.js
@@ -1,4 +1,4 @@
-const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerJsDoc = require("swagger-jsdoc");
 
 // Swagger options
 const options = {
@@ -7,16 +7,17 @@ const options = {
     info: {
       title: "Budget Planner API",
       version: "1.0.0",
-      description: "A simple express Budget Planner API that allows you to create budget envelopes as well as transactions where the transaction amount will be deducted from the corresponding envelope budget."
+      description:
+        "A simple express Budget Planner API that allows you to create budget envelopes as well as transactions where the transaction amount will be deducted from the corresponding envelope budget.",
     },
     servers: [
       {
-        url: "http://localhost:3000"
-      }
+        url: "http://localhost:3000",
+      },
     ],
   },
-  apis: ["./Server/Routes/*.js"],
-}
+  apis: ["./routes/*.js"],
+};
 
 const specs = swaggerJsDoc(options);
 
