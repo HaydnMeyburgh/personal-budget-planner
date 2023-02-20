00e0b7a2f533d512036641e8d0da555a20911a1c
Haydn <haydnmey@gmail.com>
2023-02-20 16:19:37 +0200

    created the transactions component template

---
+++ client/src/components/transactions/transactions.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Transactions = () => {
+  return (
+    <div>
+      <h2>Transactions</h2>
+      <div>
+        <h3>To:</h3>
+        <h3>Amount:</h3>
+        <h3>Date:</h3>
+        <h3>Envelope:</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Transactions;
