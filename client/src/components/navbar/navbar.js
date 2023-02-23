83eae6b3a9eca57d1f2b18793d40cca78624e68e
Haydn <haydnmey@gmail.com>
2023-02-23 11:48:55 +0200

    restructured layout and added isActive styling to NavLink

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,30 +1,51 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./navBar.css";
 
 const NavBar = () => {
   return (
     <div>
       <nav>
-        <div className="nav-items">
-          <div className="logo">
-            <a href="/">
-              <h1>Budget Planner</h1>
-            </a>
-          </div>
+        <div className="header-contents">
           <div className="docs">
             <p>
-              View the API{" "}
+              API{" "}
               <a href="http://localhost:3000/api/docs" target="_blank">
                 Documentation
               </a>
             </p>
           </div>
+          <div className="logo">
+            <a href="/">
+              <h1>Budget Planner</h1>
+            </a>
+          </div>
+        </div>
+        <div className="nav-container">
           <ul>
             <li>
-              <NavLink to="/">Envelopes</NavLink>
+              <NavLink
+                className="link"
+                to="/"
+                style={({ isActive }) => ({
+                  color: isActive ? "#fff" : "#6495ed",
+                  background: isActive ? "#6495ed" : "#f0f0f0",
+                })}
+              >
+                Envelopes
+              </NavLink>
             </li>
             <li>
-              <NavLink to="/transactions">Transactions</NavLink>
+              <NavLink
+                className="link"
+                to="/transactions"
+                style={({ isActive }) => ({
+                  color: isActive ? "#fff" : "#6495ed",
+                  background: isActive ? "#6495ed" : "#f0f0f0",
+                })}
+              >
+                Transactions
+              </NavLink>
             </li>
           </ul>
         </div>
