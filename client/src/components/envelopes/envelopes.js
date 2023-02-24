c20685824acfcb23d38bb09e68a5ad8dd01408b5
Haydn <haydnmey@gmail.com>
2023-02-24 10:03:46 +0200

    Restructured envelope layout

--- client/src/components/envelopes/envelopes.js
+++ client/src/components/envelopes/envelopes.js
@@ -1,10 +1,10 @@
 import React from "react";
+import "./envelopes.css";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const Envelopes = () => {
   const [envelopes, setEnvelopes] = useState([]);
-  // const navigate = useNavigate();
   useEffect(() => {
     const fetchEnvelopes = async () => {
       const response = await fetch("http://localhost:3000/api/envelopes");
@@ -17,15 +17,15 @@ const Envelopes = () => {
     <div className="container">
       <div className="envelopes-container">
         {envelopes.map((envelope) => (
-          <div key={envelope.id} className="envelope-card">
-            <div className="envelope-text">
-              <h3 className="envelope-title">{envelope.title}</h3>
-              <span>{envelope.budget}</span>
+          <Link to={`/envelopes/${envelope.id}`}>
+            <div key={envelope.id} className="envelope-card">
+              <div className="envelope-text">
+                <h3 className="envelope-title">{envelope.title}</h3>
+                <hr></hr>
+                <span>Budget: {envelope.budget}</span>
+              </div>
             </div>
-            <Link to={`/envelopes/${envelope.id}`}>
-              <div className="btn">View Envelope</div>
-            </Link>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
