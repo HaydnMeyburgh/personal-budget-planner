46f4e058dfe39f3749c076991929f8982bd2ad11
Haydn <haydnmey@gmail.com>
2023-03-02 14:20:45 +0200

    created the Transaction component with GET fetch request

--- client/src/components/transactions/transactions.js
+++ client/src/components/transactions/transactions.js
@@ -1,17 +1,39 @@
 import React from "react";
+import "./transactions.css";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Transactions = () => {
+  const [transactions, setTransactions] = useState([]);
+  useEffect(() => {
+      const transactionsFetch = async () => {
+        const response = await fetch("http://localhost:3000/api/transactions");
+        const transactionData = await response.json();
+        if (response.status === 200)
+        setTransactions(transactionData.data);
+      };
+    transactionsFetch();
+  }, []);
+  
   return (
-    <div>
-      <h2>Transactions</h2>
-      <div>
-        <h3>To:</h3>
-        <h3>Amount:</h3>
-        <h3>Date:</h3>
-        <h3>Envelope:</h3>
+    <>
+      <div className="container">
+        <div className="transactions-container">
+          {transactions.map((transaction) => (
+            <div key={transaction.id} className="transaction-card">
+              <Link to={`/transactions/${transaction.id}`}>
+                <div className="transaction-text">
+                  <p>Recipient: {transaction.recipient}</p>
+                  <p>Amount: {transaction.amount}</p>
+                  <p>Date: {transaction.date}</p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
-}
+};
 
 export default Transactions;
