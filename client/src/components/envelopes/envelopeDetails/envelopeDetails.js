31295901f9e5e3faa714f4797e762c8110116cf1
Haydn <haydnmey@gmail.com>
2023-03-02 12:11:08 +0200

    created a function the allows editing of envelopes

--- client/src/components/envelopes/envelopeDetails/envelopeDetails.js
+++ client/src/components/envelopes/envelopeDetails/envelopeDetails.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-// import Transactions from "../../transactions/transactions";
 
 const EnvelopeDetails = () => {
   const [envelopeDetails, setEnvelopeDetails] = useState([]);
+  const [tempEnvelope, setTempEnvelope] = useState([]);
   const [envelopeTransactions, setEnvelopeTransactions] = useState([]);
+  const [changed, setChanged] = useState(false);
   const navigate = useNavigate();
   const { id } = useParams();
+
   useEffect(() => {
     const fetchEnvelopeDetails = async () => {
       const response = await fetch(`http://localhost:3000/api/envelopes/${id}`);
       const envelopeDetailsData = await response.json();
-      setEnvelopeDetails(envelopeDetailsData.data);
+      setEnvelopeDetails(envelopeDetailsData.data[0]);
+      setTempEnvelope(envelopeDetailsData.data[0]);
     };
     const fetchTransactions = async () => {
       try {
@@ -31,20 +34,58 @@ const EnvelopeDetails = () => {
     fetchTransactions();
   }, []);
 
+  const updateEnvelope = async () => {
+    const response = await fetch(`http://localhost:3000/api/envelopes/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(tempEnvelope),
+    });
+    await response.json();
+    setEnvelopeDetails(tempEnvelope);
+    setChanged(false);
+  };
+
   return (
     <div className="container">
       <button className="btn" onClick={() => navigate(-1)}>
         Go Back
       </button>
       <div className="envelope-details-container">
-        {envelopeDetails.map((envelopeDetail) => (
-          <div key={envelopeDetail.id}>
-            <div className="envelope-details-text">
-              <h3 className="envelope-details-title">{envelopeDetail.title}</h3>
-              <span>{envelopeDetail.budget}</span>
-            </div>
+        <div key={tempEnvelope.id}>
+          <div className="envelope-details-text">
+            <input
+              className="envelope-details-title"
+              type="text"
+              value={tempEnvelope.title}
+              onChange={(e) => {
+                setChanged(true);
+                setTempEnvelope({ ...tempEnvelope, title: e.target.value });
+              }}
+            />
+            <input
+              className="envelope-details-budget"
+              type="text"
+              value={tempEnvelope.budget}
+              onChange={(e) => {
+                setChanged(true);
+                setTempEnvelope({ ...tempEnvelope, budget: e.target.value });
+              }}
+            />
           </div>
-        ))}
+        </div>
+        {changed ? (
+          <>
+            <button
+              onClick={(e) => {
+                setTempEnvelope({ ...envelopeDetails });
+                setChanged(false);
+              }}
+            >
+              Cancel
+            </button>{" "}
+            <button onClick={updateEnvelope}>Save</button>
+          </>
+        ) : null}
       </div>
       <button
         onClick={(e) => {
