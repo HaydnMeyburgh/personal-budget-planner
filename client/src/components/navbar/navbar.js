79b79b34db595e6e7a83ee986353d32b7fee5d71
Haydn <haydnmey@gmail.com>
2023-03-03 12:39:39 +0200

    restructured the navabr layout and added a paragraph

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -6,49 +6,54 @@ const NavBar = () => {
   return (
     <div>
       <nav>
+        <div className="docs">
+          <p>
+            API{" "}
+            <a href="http://localhost:3000/api/docs" target="_blank">
+              Documentation
+            </a>
+          </p>
+        </div>
         <div className="header-contents">
-          <div className="docs">
-            <p>
-              API{" "}
-              <a href="http://localhost:3000/api/docs" target="_blank">
-                Documentation
-              </a>
-            </p>
-          </div>
           <div className="logo">
             <a href="/">
               <h1>Budget Planner</h1>
             </a>
           </div>
+          <div className="nav-container">
+            <ul>
+              <li>
+                <NavLink
+                  className="link"
+                  to="/"
+                  style={({ isActive }) => ({
+                    color: isActive ? "#fff" : "#6495ed",
+                    background: isActive ? "#6495ed" : "#f0f0f0",
+                  })}
+                >
+                  Envelopes
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className="link"
+                  to="/transactions"
+                  style={({ isActive }) => ({
+                    color: isActive ? "#fff" : "#6495ed",
+                    background: isActive ? "#6495ed" : "#f0f0f0",
+                  })}
+                >
+                  Transactions
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="nav-container">
-          <ul>
-            <li>
-              <NavLink
-                className="link"
-                to="/"
-                style={({ isActive }) => ({
-                  color: isActive ? "#fff" : "#6495ed",
-                  background: isActive ? "#6495ed" : "#f0f0f0",
-                })}
-              >
-                Envelopes
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className="link"
-                to="/transactions"
-                style={({ isActive }) => ({
-                  color: isActive ? "#fff" : "#6495ed",
-                  background: isActive ? "#6495ed" : "#f0f0f0",
-                })}
-              >
-                Transactions
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+        <p className="explanation">
+          The all in one budget planner that lets you create budget envelopes to
+          track your spending, transfer budgets between envelopes, and create transactions when you spend money!
+          
+        </p>
       </nav>
     </div>
   );
