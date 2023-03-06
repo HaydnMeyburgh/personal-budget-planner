52ef8bf4f1508a32c9ff6882a7a71e4b28d5c4cd
Haydn <haydnmey@gmail.com>
2023-03-06 14:51:11 +0200

    created the files for addTransaction

---
+++ client/src/components/transactions/addTransaction/addTransaction.js
@@ -0,0 +1,57 @@
+// import React from "react";
+// import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// const AddTransaction = () => {
+//   const [recipient, setRecipient] = useState("");
+//   const [amount, setAmount] = useState("");
+//   const [date, setDate] = useState("");
+//   const navigate = useNavigate();
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const response = await fetch("http://localhost:3000/api/envelopes/", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify({
+//           title: title,
+//           budget: budget,
+//         }),
+//       });
+//       const resJson = await response.json();
+//       if (response.status === 201) {
+//         setTitle("");
+//         setBudget("");
+//       }
+//     } catch (err) {
+//       console.log(err);
+//     }
+//     navigate("/");
+//   };
+
+//   return (
+//     <div className="container">
+//       <button className="btn" onClick={() => navigate(-1)}>
+//         Go Back
+//       </button>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           value={title}
+//           placeholder="Title"
+//           onChange={(e) => setTitle(e.target.value)}
+//         />
+//         <input
+//           type="text"
+//           value={budget}
+//           placeholder="Budget"
+//           onChange={(e) => setBudget(e.target.value)}
+//         />
+//         <button type="submit">Create</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default AddTransaction;
